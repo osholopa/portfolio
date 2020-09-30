729978b935901c2b01830f1543ae1e068ea21222
Oskari Holopainen <oskariholopa@gmail.com>
2020-09-30 16:52:58 +0300

    Add email other contact links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,10 +10,29 @@ export default function Contact() {
       <Navigation />
       <Container>
         <h1>Contact information</h1>
-        <p>email: oskariholopa@gmail.com</p>
-        <p>github: https://github.com/osholopa</p>
         <p>
-          linkedin: https://www.linkedin.com/in/oskari-holopainen-0b4995191/
+          email:{' '}
+          <a href="mailto:oskariholopa@gmail.com">oskariholopa@gmail.com</a>
+        </p>
+        <p>
+          github:{' '}
+          <a
+            href="https://github.com/osholopa"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://github.com/osholopa
+          </a>
+        </p>
+        <p>
+          linkedin:{' '}
+          <a
+            href="https://www.linkedin.com/in/oskari-holopainen-0b4995191/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://www.linkedin.com/in/oskari-holopainen-0b4995191/
+          </a>
         </p>
       </Container>
     </>
