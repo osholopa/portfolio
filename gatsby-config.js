c364eecaa3f21edd26553c50d59cb7a0221c3622
Oskari Holopainen <oskariholopa@gmail.com>
2020-09-30 17:13:19 +0300

    Add manifest, offline support and page metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `osholopa`,
-    description: `A portfolio about osholopa`,
+    title: `osholopa.com`,
+    description: `Developer portfolio of Oskari Holopainen`,
     author: `Oskari Holopainen`,
   },
   plugins: [
@@ -26,5 +26,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
--- src/components/metadata.js
+++ src/components/metadata.js
@@ -1,18 +1,48 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import favicon from '../assets/favicon.png'
+import { useStaticQuery, graphql } from 'gatsby'
 
 export default function SiteMetadata({ children }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
   return (
     <div>
       <Helmet
-        title={'osholopa'}
+        lang="en"
+        title={'osholopa.com'}
         link={[
           { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           {
             rel: 'stylesheet',
             href:
               'https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap',
+          },
+        ]}
+        meta={[
+          {
+            name: `description`,
+            content: site.siteMetadata.description,
+          },
+          {
+            property: `og:title`,
+            content: site.siteMetadata.title,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
           },
         ]}
       />
