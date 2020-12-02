6f1ad82cfdfdf06c5bf6d6f8b6225b8edc312303
Oskari Holopainen <oskariholopa@gmail.com>
2020-12-02 11:54:48 +0200

    Remove phonebook and blog app

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: 'Travel-app',
-        description: 'This application uses AWS S3 bucket to store its images and MongoDB to store user and travel data. Spring security is used for authentication. Normal users can create entries, admin user can create and delete entries.',
+        description: 'This application uses AWS S3 bucket to store its images and MongoDB to store user and travel data. It uses spring security for authentication where normal users can create entries, admin users can create and delete entries.',
         link: 'https://osholopa-travel-app.herokuapp.com/',
         source: 'https://github.com/osholopa/travel-app',
         img: require('../assets/travel-app.png'),
@@ -14,22 +14,6 @@ export const projects = [
         source: 'https://github.com/osholopa/bookstore',
         img: require('../assets/bookstore.png'),
         tech: ['Java', 'Spring', 'H2', 'Thymeleaf', 'MVC']
-    },
-    {
-        title: 'Blog app',
-        description: 'This is a bloglist app where users can like and anonymously comment on blogs, authenticated users can add or remove their own blogs. The backend is a REST API that is built on top of MongoDB database.',
-        link: 'https://young-basin-26674.herokuapp.com/',
-        source: 'https://github.com/osholopa/fullStackMooc-part7',
-        img: require('../assets/bloglist.png'),
-        tech: ['React', 'Node', 'Express', 'MongoDB', 'Heroku', 'REST Api']
-    },
-    {
-        title: 'Phonebook app',
-        description: 'Full-stack phonebook app where user can add, filter and delete numbers.',
-        link: 'https://shielded-waters-62400.herokuapp.com/',
-        source: 'https://github.com/osholopa/fullStackMooc-part3',
-        img: require('../assets/puhelinluettelo.png'),
-        tech: ['React', 'Node', 'Express', 'MongoDB', 'Heroku']
     },
     {
         title: 'Button click game',
